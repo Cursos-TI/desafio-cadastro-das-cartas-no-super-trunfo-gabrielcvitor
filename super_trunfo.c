a664604df26c4cef5490c52c256e352cebd86d35
gabrielcvitor <gabrielvitor4141@gmail.com>
2025-03-20 00:47:31 -0300

    Create super_trunfo.c
    Digitação inicial do código

---
+++ super_trunfo.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main (){
+
+char estado[1];
+char codigodacarta[3];
+char cidade[30];
+int populacao;
+float area;
+float pib;
+int pontosturisticos;
+  
+
+
+printf("Super Trunfo \n");
+
+printf("Insira os dados das cidades em suas respectivas cartas: \n");
+
+printf("Carta 1 \n");
+
+printf("Digite estado a que a cidade pertence: \n");
+scanf("%d", &estado);
+
+printf("Crie um código para a carta começando pela letra maiúscula que representa o estado seguida de um número de 01 a 04: \n");
+scanf("%s", &codigodacarta);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &cidade);
+
+printf("Digite o número de habitantes da cidade: \n");
+scanf("%d", &populacao);
+
+printf("Digite a área da cidade em km²:\n");
+scanf("%f", &area);
+
+printf("Digite o PIB da cidade: \n");
+scanf("%f", &pib);
+
+printf("Digite o número de pontos turísticos da cidade: \n");
+scanf("%d", &pontosturisticos);
+
+  printf("Carta 1:");
+  printf("Estado: %d\n", estado);
+  printf("Código: %s\n", codigodacarta);
+  printf("Nome da cidade: %s\n", cidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %f km²\n", area);
+  printf("PIB: %f\n", pib);
+  printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
+
+return 0;
+}
