b7ce0834f60e3fbb89fbecb0305e7da8dde27ed2
gabrielcvitor <gabrielvitor4141@gmail.com>
2025-03-26 02:02:56 -0300

    Update super_trunfo.2.0.c
    Foram feitas correções nas variáveis e especificadores char; onde estava '%d' foi substituído por '%s".
    Nas variáveis de PIB e área foram adicionadas formatações decimais. 
    Próximo passo é a conferência dos textos do questionário.

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main (){
-//nao está funcionandoa linha 22 
-char estado[1];a 
+char estado[2];
 char codigodacarta[3];
 char cidade[30];
-int populacao;
-float area;
-float pib;
-int pontosturisticos;
+int populacao = 1234;
+float area = 4321;
+float pib = 134;
+int pontosturisticos = 159753;
   
 
 
@@ -18,13 +17,13 @@ printf("Insira os dados das cidades em suas respectivas cartas: \n");
 printf("Carta 1 \n");
 
 printf("Digite estado a que a cidade pertence: \n");
-scanf("%d", &estado);
+scanf("%s", estado);
 
-printf("Crie um código para a carta começando pela letra maiúscula que representa o estado seguida de um número de 01 a 04: \n");
-scanf("%s", &codigodacarta);
+printf("Crie um código para identificação da carta, ele deve começar pela letra maiúscula que representa o estado, seguida de um número de dois digitos de 01 a 04: \n");
+scanf("%s", codigodacarta);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", &cidade);
+scanf("%s", cidade);
 
 printf("Digite o número de habitantes da cidade: \n");
 scanf("%d", &populacao);
@@ -39,24 +38,24 @@ printf("Digite o número de pontos turísticos da cidade: \n");
 scanf("%d", &pontosturisticos);
 
   printf("Carta 1:");
-  printf("Estado: %d\n", estado);
+  printf("Estado: %s\n", estado);
   printf("Código: %s\n", codigodacarta);
   printf("Nome da cidade: %s\n", cidade);
   printf("População: %d\n", populacao);
-  printf("Área: %f km²\n", area);
-  printf("PIB: %f\n", pib);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: %.2f\n", pib);
   printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
  
   printf("Carta 2 \n");
 
   printf("Digite estado a que a cidade pertence: \n");
-  scanf("%d", &estado);
+  scanf("%s", estado);
 
   printf("Crie um código para a carta começando pela letra maiúscula que representa o estado seguida de um número de 01 a 04: \n");
-  scanf("%s", &codigodacarta);
+  scanf("%s", codigodacarta);
 
   printf("Digite o nome da cidade: \n");
-  scanf("%s", &cidade);
+  scanf("%s", cidade);
 
   printf("Digite o número de habitantes da cidade: \n");
   scanf("%d", &populacao);
@@ -71,12 +70,12 @@ scanf("%d", &pontosturisticos);
   scanf("%d", &pontosturisticos);
 
   printf("Carta 2:\n");
-  printf("Estado: %d\n", estado);
+  printf("Estado: %s\n", estado);
   printf("Código: %s\n", codigodacarta);
   printf("Nome da cidade: %s\n", cidade);
   printf("População: %d\n", populacao);
-  printf("Área: %f km²\n", area);
-  printf("PIB: %f\n", pib);
+  printf("Área: %.f km²\n", area);
+  printf("PIB: %.2f\n", pib);
   printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
 return 0;
 }
